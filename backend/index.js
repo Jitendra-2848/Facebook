33d7afcfa30ad1eef04fae4c8be2a8a0c461103c
Jitendra Prajapati <prajapatijitendra2848@gmail.com>
2025-10-17 19:48:32 +0530

    mongo

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ server.use(cors({
 }));
 server.use(express.json());
 
-mongo.connect(`mongodb+srv://prajapatijitendra2848_db_user:rQr2nn6xL2HbZRjg@e-commerce.95z2us2.mongodb.net/?retryWrites=true&w=majority&appName=E-commerce`)
+mongo.connect(`${process.env.MONGO_URL}`)
     .then(() => console.log("MongoDB connected..."))
     .catch((e) => console.error("Mongo error:", e));
 
