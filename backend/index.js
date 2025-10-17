2c36bce728d0ac4f351656f7cd15f073f7bb860b
Jitendra Prajapati <prajapatijitendra2848@gmail.com>
2025-10-17 18:47:27 +0530

    Trial 2

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,29 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const route = require("./route/user");
-const server = express();
-const mongo = require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const server = express();
+
 server.use(cookieParser());
 server.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-}))
-mongo.connect(process.env.mongo)
-.then(()=>{
-    console.log("connected!");
-})
-.catch((e)=>{
-    console.log("something went wrong " + e.message)
-})
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 server.use(express.json());
- server.use(route)
-// server.use("/",(req,res)=>{
-//     res.send("hello brother");
-// })
 
-server.listen(3000,()=>{
-    console.log("hello");
+mongoose.connect(process.env.mongo)
+.then(() => {
+  console.log("connected!");
 })
+.catch((e) => {
+  console.log("something went wrong " + e.message);
+});
+
+server.use(route);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("hello");
+});
